deee17cb930dba7fcf6e931594fa01171c354ea4
Kevin Guan <20823498+guan-kevin@users.noreply.github.com>
2024-12-29 23:19:18 -0800

    Create post.js

---
+++ post.js
@@ -0,0 +1,15 @@
+import {DefaultArtifactClient} from '@actions/artifact'
+
+const {id, size} = await artifact.uploadArtifact(
+  // name of the artifact
+  'my-artifact',
+  // files to include (supports absolute and relative paths)
+  ['/absolute/path/file1.txt', './relative/file2.txt'],
+  {
+    // optional: how long to retain the artifact
+    // if unspecified, defaults to repository/org retention settings (the limit of this value)
+    retentionDays: 10
+  }
+)
+
+console.log(`Created artifact with id: ${id} (bytes: ${size}`)
