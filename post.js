0998c83042d3c74f0899489abd62996e7a1fb752
Kevin Guan <20823498+guan-kevin@users.noreply.github.com>
2024-12-30 00:23:12 -0800

    Update post.js

--- post.js
+++ post.js
@@ -1,20 +1,13 @@
 async function main() {
-  console.log('test 1')
-
   const {DefaultArtifactClient} = require('@actions/artifact')
-  
-  console.log('test 2')
-
   const artifact = new DefaultArtifactClient()
   const {id, size} = await artifact.uploadArtifact(
     // name of the artifact
     'traces',
     // files to include (supports absolute and relative paths)
     ['./traces.tar.gz'],
-    '/'
+    '.'
   )
-  
-  console.log(`Created artifact with id: ${id} (bytes: ${size}`)
 }
 
 main();
