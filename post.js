1d002e331349a337dba1b086deea72832dfe5e7e
Kevin Guan <20823498+guan-kevin@users.noreply.github.com>
2024-12-29 23:28:22 -0800

    Update post.js

--- post.js
+++ post.js
@@ -1,19 +1,23 @@
-console.log('test 1')
+async function main() {
+  console.log('test 1')
 
-const {DefaultArtifactClient} = require('@actions/artifact')
+  const {DefaultArtifactClient} = require('@actions/artifact')
+  
+  console.log('test 2')
+  
+  const {id, size} = await artifact.uploadArtifact(
+    // name of the artifact
+    'my-artifact',
+    // files to include (supports absolute and relative paths)
+    ['/absolute/path/file1.txt', './relative/file2.txt'],
+    {
+      // optional: how long to retain the artifact
+      // if unspecified, defaults to repository/org retention settings (the limit of this value)
+      retentionDays: 10
+    }
+  )
+  
+  console.log(`Created artifact with id: ${id} (bytes: ${size}`)
+}
 
-console.log('test 2')
-
-const {id, size} = await artifact.uploadArtifact(
-  // name of the artifact
-  'my-artifact',
-  // files to include (supports absolute and relative paths)
-  ['/absolute/path/file1.txt', './relative/file2.txt'],
-  {
-    // optional: how long to retain the artifact
-    // if unspecified, defaults to repository/org retention settings (the limit of this value)
-    retentionDays: 10
-  }
-)
-
-console.log(`Created artifact with id: ${id} (bytes: ${size}`)
+main();
